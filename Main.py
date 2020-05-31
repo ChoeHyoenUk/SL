fc76b2412c7e6c76f50a804e6644834e1e1b8952
ChoeHyoenUk <mrbear6500@gmail.com>
2020-05-31 19:08:59 +0900

    배우 검색 기능
    배우 검색, 출연작 검색, 출연작 세부정보

--- Main.py
+++ Main.py
@@ -38,7 +38,7 @@ class MovieApp:
         # 박스오피스 랭킹 페이지
         self.rankingPage = Frame(window, width=900, height=650, bg='white')
         self.detailInfo = []
-        self.posterImage = []
+        self.rankingPosterImage = []
         self.pageNum = 0
         self.periodComboBox = ttk.Combobox(self.rankingPage, width=10)
         self.periodComboBox['value'] = ['일간', '주간']
@@ -60,25 +60,32 @@ class MovieApp:
         # 배우/감독 검색 페이지
         self.searchPage = Frame(window, width=900, height=650, bg='white')
         self.searchElementComboBox = ttk.Combobox(self.searchPage, width=10)
-        self.searchElementComboBox['value'] = ['배우', '감독']
+        self.searchElementComboBox['value'] = ['영화', '배우/감독']
         self.searchElementComboBox.current(0)
         self.searchElementComboBox.place(x=0, y=0)
         self.nameEntry = Entry(self.searchPage, width=55, relief='solid', bd=2)
-        self.nameEntry.insert(END, "배우/감독의 이름 입력")
+        self.nameEntry.insert(END, "영화제목/배우/감독의 이름 입력")
         self.nameEntry.place(x=100, y=0)
-        self.nameSearchButton = Button(self.searchPage, text='검색', width=5)
+        self.nameSearchButton = Button(self.searchPage, text='검색', width=5, command=self.NameSearch)
         self.nameSearchButton.place(x=500, y=0)
+        self.actorAndDirectorListbox = Listbox(self.searchPage, width=50, height=35, relief='solid', bd=5)
+        self.nameSearchResultList = []
+        self.filmoListbox = Listbox(self.searchPage, width=50, height=35, relief='solid', bd=5)
+        self.filmoList = []
+        self.selectedName = None
+        self.moviePoster = None
+        self.viewFilmoButton = Button(self.searchPage, text='조회', width=5, command=self.ViewFilmo)
+        self.viewDetailInfoButton = Button(self.searchPage, text='세부 정보', command=self.ShowDetailInfo)
         self.searchPage.place(x=0, y=100)
 
         # 영화관 검색 페이지
         self.theaterPage = Frame(window, width=900, height=650, bg='white')
-        self.nameEntry = Entry(self.theaterPage, width=55, relief='solid', bd=2)
-        self.nameEntry.insert(END, "시/군 입력(경기도에 위치한 시/군만 검색 가능합니다.)")
-        self.nameEntry.place(x=0, y=0)
-        self.nameSearchButton = Button(self.theaterPage, text='검색', width=5)
-        self.nameSearchButton.place(x=500, y=0)
+        self.placeEntry = Entry(self.theaterPage, width=70, relief='solid', bd=2)
+        self.placeEntry.insert(END, "시/군 입력(경기도에 위치한 시/군만 검색 가능합니다.)")
+        self.placeEntry.place(x=0, y=0)
+        self.placeSearchButton = Button(self.theaterPage, text='검색', width=5)
+        self.placeSearchButton.place(x=500, y=0)
         self.theaterPage.place(x=0, y=100)
-
 
         # 처음 시작시 랭킹 페이지가 나오도록 설정함
         self.rankingPage.tkraise()
@@ -96,14 +103,13 @@ class MovieApp:
 
     def RankingSearch(self):
         s = self.periodComboBox.get()
-        self.posterImage.clear()
+        self.rankingPosterImage.clear()
         self.detailInfo.clear()
         self.pageNum = 0
-
         if s == '일간':
             tree = DailyRanking(self.periodEntry.get())
             items = tree.iter('dailyBoxOffice')
-        elif s == '주간':
+        else:
             tree = WeaklyRanking(self.periodEntry.get())
             items = tree.iter('weeklyBoxOffice')
 
@@ -120,18 +126,18 @@ class MovieApp:
                      '누적 매출: ' + sales + '\n' + \
                      '누적 관객: ' + audi
             self.detailInfo.append(detail)
-            url = GetPosterURL(name, openDt)
+            url = GetPosterURL_openDt(name, openDt)
 
             if url != 'NoImage':
                 with urllib.request.urlopen(url) as u:
                     data = u.read()
                 im = Image.open(BytesIO(data))
-                self.posterImage.append((name, ImageTk.PhotoImage(im)))
+                self.rankingPosterImage.append((name, ImageTk.PhotoImage(im)))
             else:
-                self.posterImage.append((name, PhotoImage(file='Images/NoImage.png')))
+                self.rankingPosterImage.append((name, PhotoImage(file='Images/NoImage.png')))
 
         for i in range(3):
-            self.posterButton[i].configure(text=self.posterImage[i][0], image=self.posterImage[i][1])
+            self.posterButton[i].configure(text=self.rankingPosterImage[i][0], image=self.rankingPosterImage[i][1])
             self.posterButton[i].place(x=80 + (250 * i), y=150)
 
         self.nextPage.place(x=850, y=320)
@@ -139,36 +145,78 @@ class MovieApp:
         self.nextPage['state'] = 'active'
         self.prevPage['state'] = 'disable'
 
+    def NameSearch(self):
+        case = self.searchElementComboBox.get()
+        if case == '영화':
+            title = self.nameEntry.get()
+        else:
+            name = self.nameEntry.get()
+            self.nameSearchResultList = GetActorAndDirector(name)
+            self.actorAndDirectorListbox.delete(0, END)
+            for people in self.nameSearchResultList:
+                self.actorAndDirectorListbox.insert(END, people[0])
+            self.actorAndDirectorListbox.place(x=20, y=50)
+            self.viewFilmoButton.place(x=390, y=320)
+
     def ShowNextPage(self):
         self.pageNum += 1
         self.prevPage['state'] = 'active'
         if self.pageNum == 3:
             self.nextPage['state'] = 'disable'
-            self.posterButton[0].configure(text=self.posterImage[9][0],
-                                           image=self.posterImage[9][1])
+            self.posterButton[0].configure(text=self.rankingPosterImage[9][0],
+                                           image=self.rankingPosterImage[9][1])
             self.posterButton[1].configure(text='BLANK',
                                            image=PhotoImage(file='Images/NoImage.png'))
             self.posterButton[2].configure(text='BLANK',
                                            image=PhotoImage(file='Images/NoImage.png'))
         else:
             for i in range(3):
-                self.posterButton[i].configure(text=self.posterImage[i + (3 * self.pageNum)][0],
-                                               image=self.posterImage[i + (3 * self.pageNum)][1])
+                self.posterButton[i].configure(text=self.rankingPosterImage[i + (3 * self.pageNum)][0],
+                                               image=self.rankingPosterImage[i + (3 * self.pageNum)][1])
 
     def ShowPrevPage(self):
         self.pageNum -= 1
         self.nextPage['state'] = 'active'
         for i in range(3):
-            self.posterButton[i].configure(text=self.posterImage[i + (3 * self.pageNum)][0],
-                                           image=self.posterImage[i + (3 * self.pageNum)][1])
+            self.posterButton[i].configure(text=self.rankingPosterImage[i + (3 * self.pageNum)][0],
+                                           image=self.rankingPosterImage[i + (3 * self.pageNum)][1])
         if self.pageNum == 0:
             self.prevPage['state'] = 'disable'
 
     def ShowMovieInfo(self, idx):
         self.canvas.delete('all')
-        self.canvas.create_image(250, 150, image=self.posterImage[idx + (3 * self.pageNum)][1])
+        self.canvas.create_image(250, 150, image=self.rankingPosterImage[idx + (3 * self.pageNum)][1])
         self.infoText.delete(1.0, END)
         self.infoText.insert(END, self.detailInfo[idx+(3*self.pageNum)])
+
+    def ViewFilmo(self):
+        code = self.nameSearchResultList[self.actorAndDirectorListbox.curselection()[0]][1]
+        self.selectedName = self.nameSearchResultList[self.actorAndDirectorListbox.curselection()[0]][0]
+        self.filmoList = GetFilmo(code)
+        self.filmoListbox.delete(0, END)
+        for filmo in self.filmoList:
+            self.filmoListbox.insert(END, filmo[0])
+        self.filmoListbox.place(x=450, y=50)
+        self.viewDetailInfoButton.place(x=820, y=320)
+
+    def ShowDetailInfo(self):
+        title = self.filmoList[self.filmoListbox.curselection()[0]][0]
+        code = self.filmoList[self.filmoListbox.curselection()[0]][1]
+        url = GetPosterURL_actor(title, self.selectedName)
+        if url != 'NoImage':
+            with urllib.request.urlopen(url) as u:
+                data = u.read()
+            im = Image.open(BytesIO(data))
+            self.moviePoster = ImageTk.PhotoImage(im)
+            self.canvas.delete('all')
+            self.canvas.create_image(250, 150, image=self.moviePoster)
+        else:
+            self.moviePoster = PhotoImage(file='Images/NoImage.png')
+            self.canvas.delete('all')
+            self.canvas.create_image(250, 150, image=self.moviePoster)
+        DetailInfo = GetDetailInfo(code)
+        self.infoText.delete(1.0, END)
+        self.infoText.insert(END, DetailInfo)
 
 
 MovieApp()
--- xmlFunc.py
+++ xmlFunc.py
@@ -1,5 +1,6 @@
 from xml.etree import ElementTree
 import requests
+import urllib
 
 kobisKey = '23534542e0be066999d2803d1057fc37'
 kmdbKey = 'N1UG972286869QC55WOA'
@@ -23,7 +24,7 @@ def WeaklyRanking(period):
     return tree
 
 
-def GetPosterURL(title, releaseDts):
+def GetPosterURL_openDt(title, releaseDts):
     url = 'http://api.koreafilm.or.kr/openapi-data2/wisenut/search_api/search_xml2.jsp?collection=kmdb_new2'
     url += '&ServiceKey=' + kmdbKey + '&title=' + title + '&releaseDts=' + releaseDts
     res = requests.get(url).text
@@ -38,3 +39,90 @@ def GetPosterURL(title, releaseDts):
             else:
                 return posterURL
     return 'NoImage'
+
+
+def GetPosterURL_actor(title, actor):
+    url = 'http://api.koreafilm.or.kr/openapi-data2/wisenut/search_api/search_xml2.jsp?collection=kmdb_new2'
+    url += '&ServiceKey=' + kmdbKey + '&title=' + title + '&actor=' + actor
+    res = requests.get(url).text
+    tree = ElementTree.fromstring(res)
+    items = tree.iter('Row')
+    for item in items:
+        posterURL = item.find('posters').text.split('|')[0].replace(' ', '')
+        if posterURL == '':
+            return 'NoImage'
+        else:
+            return posterURL
+    return "NoImage"
+
+
+def GetActorAndDirector(name):
+    url = 'http://www.kobis.or.kr/kobisopenapi/webservice/rest/people/searchPeopleList.xml'
+    url += '?key=' + kobisKey + '&peopleNm=' + name + '&itemPerPage=100'
+    res = requests.get(url).text
+    tree = ElementTree.fromstring(res)
+    items = tree.iter('people')
+    l = []
+    for item in items:
+        role = item.find('repRoleNm').text
+        if role == '감독' or role == '배우':
+            peopleName = item.find('peopleNm').text
+            peopleCode = item.find('peopleCd').text
+            l.append((peopleName, peopleCode))
+    return l
+
+
+def GetFilmo(code):
+    url = 'http://www.kobis.or.kr/kobisopenapi/webservice/rest/people/searchPeopleInfo.xml'
+    url += '?key=' + kobisKey + '&peopleCd=' + code
+    res = requests.get(url).text
+    tree = ElementTree.fromstring(res)
+    items = tree.iter('filmo')
+    l = []
+    for item in items:
+        movieNm = item.find('movieNm').text
+        movieCd = item.find('movieCd').text
+        l.append((movieNm, movieCd))
+    return l
+
+
+def GetDetailInfo(code):
+    url = 'http://www.kobis.or.kr/kobisopenapi/webservice/rest/movie/searchMovieInfo.xml'
+    url += '?key=' + kobisKey + '&movieCd=' + code
+    res = requests.get(url).text
+    tree = ElementTree.fromstring(res)
+    infoTree = tree.iter('movieInfo')
+    for info in infoTree:
+        movieNm = '제목: ' + info.find('movieNm').text
+        runtime = '상영 시간: ' + info.find('showTm').text
+        openDt = '개봉일: ' + info.find('openDt').text
+
+        genreTree = info.iter('genre')
+        genre = '장르: '
+        for g in genreTree:
+            genre += g.find('genreNm').text
+
+        directorTree = info.iter('director')
+        director = '감독: '
+        for d in directorTree:
+            director += d.find('peopleNm').text
+
+        actorTree = list(info.iter('actor'))
+        actor = '출연 배우: '
+        for i in range(3):
+            actor += actorTree[i].find('peopleNm').text + ' '
+
+        auditTree = info.iter('audit')
+        watchGradeNm = '관람 등급: '
+        for w in auditTree:
+            watchGradeNm += w.find('watchGradeNm').text
+
+        DetailInfoStr = movieNm + '\n' + \
+                        runtime + '\n' + \
+                        openDt + '\n' + \
+                        genre + '\n' + \
+                        director + '\n' + \
+                        actor + '\n' + \
+                        watchGradeNm
+
+    return DetailInfoStr
